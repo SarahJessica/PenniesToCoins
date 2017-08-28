02618ce8cd71b006a3df6e1d28219acf5c4e33bd
Jessica Leivers <sarahjessicaleivers@gmail.com>
2017-08-28 22:04:16 +0100

    basic functioning, buggy, not working on enter press

--- src/js/calculations.js
+++ src/js/calculations.js
@@ -8,12 +8,64 @@ function init() {
 }
 
 $('#go').click(function() {
+  var radio = $("input[name='gender']:checked").attr('value');
+  var currency = getDenominations(radio);
+  // var currency = $("input[name='gender']:checked").val();
   var num = $('#p').val();
-  console.log(num);
-  $('#answer').append(num);
+  var ans = mainCalculation(num, currency);
+  console.log(ans);
+  displayAnswers(currency, ans);
+  $('#resultDisplay').append(displayAnswers(currency, ans));
 });
 
-// function mainCalculation() {
-//   var num = $('#penniesTotal').val();
-//   $('#answer').append(num);
+const sterlingDenominations = [200, 100, 50, 20, 10, 5, 2, 1];
+const usdDenominations = [100, 50, 25, 10, 5, 1];
+const euroDenominations = [200, 100, 50, 20, 10, 5, 2, 1];
+
+function getDenominations(radioValue) {
+  console.log(radioValue);
+  switch (radioValue) {
+    case "GBP":
+      return sterlingDenominations;
+      break;
+    case "USD":
+      return usdDenominations;
+      break;
+    case "EUR":
+      return euroDenominations;
+      break;
+    default:
+      return sterlingDenominations;
+  }
+}
+
+function mainCalculation(i, demonimations) {
+  var value = parseInt(i);
+  return demonimations.map(function(d) {
+    var coinCount = 0;
+    if (value >= d) {
+      var coinCount = Math.floor(value / d);
+      value = value - (coinCount * d);
+    }
+    return coinCount
+  });
+}
+
+function displayAnswers(denominations, mappedAnswerArray) {
+  $('#resultDisplay').empty();
+  var answerList = $();
+  var colWidth;
+  if (denominations == [100, 50, 25, 10, 5, 1]) {
+    colWidth = 'col-sm-2';
+  } else {
+    colWidth = 'col-sm-3';
+  }
+  denominations.map(function(e, i) {
+    answerList = answerList.add('<div class="coin">' + e + ' <br /> '+ mappedAnswerArray[i] + '</div>');
+  });
+  $('#resultDisplay').append(answerList);
+}
+
+// function test(a, b, c) {
+//   return a + b + c;
 // }
